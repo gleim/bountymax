8e9c5597178eb8f7383f6fc935536141fd9208a3
andysign <andysignerdesigner@gmail.com>
2016-09-11 13:34:26 +0100

    How to exploit the beer contract from the javascript web compiler.

---
+++ contracts/TargetBeer.sol
@@ -0,0 +1,7 @@
+// The best way to exploit the b contract
+// is to call the function tap with an argument 
+// string more than 20 b
+// for example call tap('123456789012345678901')
+// using the JavaScript Web Sol compiler
+// At Address 0xc78e97b464d26687811db32140724cc426fe0b62
+// tap "123456789012345678901"
